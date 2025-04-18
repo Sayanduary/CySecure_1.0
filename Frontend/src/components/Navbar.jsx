8666a8d7b59bd87a8f1e1c1751917b8b90c94e79
Atul Kumar Chaurasia <atulkumarchourasia64796@gmail.com>
2025-04-18 17:12:24 +0530

    design navbar in home page

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 function Navbar() {
   return (
-    <div className=' flex fixed w-[100%] '>
-      <div className='w-1/2 bg-gray-700/35 py-4 px-7 '> <span className='text-4xl  bg-gradient-to-tr from-purple-400 via-white to-black bg-clip-text text-transparent font-bold'>Cysecure</span></div>
-      <ul className='bg-gray-700/35 flex justify-between gap-2  4 px-9 w-1/2 items-center'>
+    <div className=' flex fixed w-[100%] z-[10]'>
+      <div className='w-1/2 bg-gray-800 py-4 px-7 '> <span className='text-4xl  bg-gradient-to-tr from-purple-400 via-white to-black bg-clip-text text-transparent font-bold'>Cysecure</span></div>
+      <ul className='bg-gray-800 flex justify-between gap-2  4 px-9 w-1/2 items-center'>
         <NavLink to="/" className={(e)=>e.isActive?" bg-green-600 text-white font-bold":"text-white"}><li className='p-4 '>Home</li></NavLink>
         <NavLink to="/dashboard" className={(e)=>e.isActive?"bg-green-600 font-bold text-white":"text-white"} ><li className='p-4'>Dashboard</li></NavLink>
         <NavLink to="/logs"  className={(e)=>e.isActive?"bg-green-600 font-bold text-white":"text-white"} ><li className='p-4'>Logs</li></NavLink>
