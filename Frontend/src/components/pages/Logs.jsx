4758ff6cc479ca1750bd07183c7311be081b6517
Atul Kumar Chaurasia <atulkumarchourasia64796@gmail.com>
2025-04-21 00:55:47 +0530

    fetch data from backend

--- Frontend/src/components/pages/Logs.jsx
+++ Frontend/src/components/pages/Logs.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useEffect,useState } from 'react'
+// import { data } from 'react-router-dom'
 function Logs() {
+const [Data , setData] = useState([])
+
+useEffect(()=>{
+axios.get('http://localhost:8000/api/v1/packets')
+.then((res)=>{
+console.log(res.data[0])
+setData(res.data)
+}).catch((error)=>{
+console.error('Error fetching data',error)
+})
+},[])
+
   return (
-    <div>
-      <h1>logs page</h1>
-    </div>
+    <>
+    <div className='flex justify-center items-center   h-[100vh]'>
+   <div className='flex justify-center items-center bg-gray-500 '>{Data.map((e,index)=>(
+    <div key={index} className='flex  gap-40'>
+      <p>{e.destinationIP}</p>
+      <p >{e.destinationPort}</p>
+      <p>{e.protocol}</p>
+      <p>{e.size}</p>
+      <p>{e.sourceIP}</p>
+      <p>{e.sourcePort}</p>
+      <p>{e.timestamp}</p>
+      </div>
+        ))}
+        </div>
+        </div>
+    </>
   )
 }
 
